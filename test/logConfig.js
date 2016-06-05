8f6aaba1a60046e2e4482b8eedfc645d674a4508
blndev <daniel>
2016-06-05 20:58:28 +0200

    introduce mocha and chai for testing

---
+++ test/logConfig.js
@@ -0,0 +1,22 @@
+'use strict';
+var assert = require('chai').assert;
+var clc = require('cli-color');
+
+describe('log config handling', function () {
+
+    var log;
+    before(function () {
+        log = require('../index.js')();
+    });
+
+    describe('when modify colors',
+        function () {
+            it('then get should return the value created by set', function () {
+                var testColor = clc.magenta;
+                var level = log.logLevels.warn;
+                assert.notEqual(log.getColor(level), testColor, 'color is not different to test value, modify test!');
+                log.setColor(level, testColor);
+                assert.equal(log.getColor(level), testColor, 'color is not modified');
+            });
+        });
+});
